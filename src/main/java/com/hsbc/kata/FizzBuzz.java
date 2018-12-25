88480719516b9dabb5a03a5712dbd7d12c7dcbbd
zach007 <zach007cn@gmail.com>
2018-12-25 23:30:25 +0800

    code refactor fizz Buzz

--- src/main/java/com/hsbc/kata/FizzBuzz.java
+++ src/main/java/com/hsbc/kata/FizzBuzz.java
@@ -3,11 +3,13 @@ package com.hsbc.kata;
 public class FizzBuzz {
 
     public String checkNumber(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
+        boolean multiplesOf3 = number % 3 == 0;
+        boolean multiplesOf5 = number % 5 == 0;
+        if (multiplesOf3 && multiplesOf5) {
             return "fizzBuzz";
-        } else if (number % 3 == 0) {
+        } else if (multiplesOf3) {
             return "fizz";
-        } else if (number % 5 == 0) {
+        } else if (multiplesOf5) {
             return "buzz";
         } else {
             return String.valueOf(number);
