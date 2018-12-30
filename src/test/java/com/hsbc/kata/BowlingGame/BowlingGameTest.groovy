7862d9ed94bbefffc04d3ad7586cf25c15f94632
zach007 <zach007cn@gmail.com>
2018-12-30 19:18:51 +0800

    5c23871e99acea55d1987c64 code kata : bowling game refactor code

--- src/test/java/com/hsbc/kata/BowlingGame/BowlingGameTest.groovy
+++ src/test/java/com/hsbc/kata/BowlingGame/BowlingGameTest.groovy
@@ -5,12 +5,6 @@ import spock.lang.Specification
 class BowlingGameTest extends Specification {
     BowlingGame game = new BowlingGame()
 
-    private void rollManyTimes(int times, int pins) {
-        for (int i = 0; i < times; i++) {
-            game.roll(pins)
-        }
-    }
-
     def "should return 20 when call 20 times of roll()"() {
         when:
         rollManyTimes(20, 1)
@@ -27,8 +21,7 @@ class BowlingGameTest extends Specification {
 
     def "should return 16 when call a spare"() {
         when:
-        game.roll(5)
-        game.roll(5)
+        rollSpare()
         game.roll(3)
         rollManyTimes(17, 0)
         then:
@@ -37,11 +30,33 @@ class BowlingGameTest extends Specification {
 
     def "should return 24 when call a strike"() {
         when:
-        game.roll(10)
+        rollStrike()
         game.roll(3)
         game.roll(4)
         rollManyTimes(16, 0)
         then:
         game.score() == 24
+    }
+
+    def "should return 300 when call a perfect game"() {
+        when:
+        rollManyTimes(20, 10)
+        then:
+        game.score() == 300
+    }
+
+    private void rollManyTimes(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins)
+        }
+    }
+
+    private void rollSpare() {
+        game.roll(5)
+        game.roll(5)
+    }
+
+    private rollStrike() {
+        game.roll(10)
     }
 }
