47e339afd3ff6795a5f96349be91e29fc61d655f
zach007 <zach007cn@gmail.com>
2019-01-17 23:56:00 +0800

    5c3de98d1029fd3351007697 gilded rose init version

--- src/test/java/com/hsbc/kata/gildedrose/GildedRoseTest.groovy
+++ src/test/java/com/hsbc/kata/gildedrose/GildedRoseTest.groovy
@@ -3,4 +3,16 @@ package com.hsbc.kata.gildedrose
 import spock.lang.Specification
 
 class GildedRoseTest extends Specification {
+
+    def "foo"() {
+        Item[] items = [new Item("foo", 0, 0)]
+        GildedRose app = new GildedRose(items)
+        app.updateQuality()
+        expect:
+        "fixme" == app.items[0].name
+    }
+
+    def "quality should never more than 50"() {
+
+    }
 }
