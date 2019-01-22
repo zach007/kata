218e9324d4ac953869e7a6f1c1108d97d5d745b9
zach007 <zach007cn@gmail.com>
2019-01-22 23:37:14 +0800

    5c3de98d1029fd3351007697 gilded rose add test case

--- src/test/java/com/hsbc/kata/gildedrose/GildedRoseTest.groovy
+++ src/test/java/com/hsbc/kata/gildedrose/GildedRoseTest.groovy
@@ -9,10 +9,56 @@ class GildedRoseTest extends Specification {
         GildedRose app = new GildedRose(items)
         app.updateQuality()
         expect:
-        "fixme" == app.items[0].name
+        "foo" == app.items[0].name
     }
 
-    def "quality should never more than 50"() {
+    def "Once the sell by date has passed, Quality degrades twice as fast"() {
+        Item[] items = [new Item("foo", 3, 10)]
+        def app = new GildedRose(items)
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality()
+        }
+        def quality = app.items[0].quality
+        expect:
+        quality == 3
+    }
+
+    def "The Quality of an item is never negative"() {
+        Item[] items = [new Item("foo", 1, 10)]
+        def app = new GildedRose(items)
+        for (int i = 0; i < 10; i++) {
+            app.updateQuality()
+        }
+        def quality = app.items[0].quality
+        expect:
+        quality == 0
+    }
+
+    def "“Aged Brie” actually increases in Quality the older it gets"() {
+        Item[] items = [new Item("Aged Brie", 3, 10)]
+        def app = new GildedRose(items)
+        for (int i = 0; i < 10; i++) {
+            app.updateQuality()
+        }
+        def quality = app.items[0].quality
+        println(quality)
+        expect:
+        quality == 27
+    }
+
+    def "The Quality of an item is never more than 50"() {
+        Item[] items = [new Item("Aged Brie", 3, 10)]
+        def app = new GildedRose(items)
+        for (int i = 0; i < 50; i++) {
+            app.updateQuality()
+        }
+        def quality = app.items[0].quality
+        println(quality)
+        expect:
+        quality == 50
+    }
+
+    def "“Sulfuras”, being a legendary item, never has to be sold or decreases in Quality"() {
 
     }
 }
