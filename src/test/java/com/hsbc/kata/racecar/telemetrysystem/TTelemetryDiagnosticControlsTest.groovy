eb37973d48af8cbce30626b965057c992d58c532
zach007 <zach007cn@gmail.com>
2019-02-08 23:48:31 +0800

    5c5c4fe8771d8671d4f14fb4 race car add test

---
+++ src/test/java/com/hsbc/kata/racecar/telemetrysystem/TTelemetryDiagnosticControlsTest.groovy
@@ -0,0 +1,7 @@
+package com.hsbc.kata.racecar.telemetrysystem
+
+import spock.lang.Specification
+
+class TTelemetryDiagnosticControlsTest extends Specification {
+    def telemetryDiagnosticControls = new TelemetryDiagnosticControls()
+}
