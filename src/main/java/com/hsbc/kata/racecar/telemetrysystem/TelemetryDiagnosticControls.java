9a459743f3685d39f8ec38098dc0c02b71c0dec0
zach007 <zach007cn@gmail.com>
2019-02-23 23:47:52 +0800

    5c5c4fe8771d8671d4f14fb4 race car refactor code

--- src/main/java/com/hsbc/kata/racecar/telemetrysystem/TelemetryDiagnosticControls.java
+++ src/main/java/com/hsbc/kata/racecar/telemetrysystem/TelemetryDiagnosticControls.java
@@ -17,12 +17,13 @@ public class TelemetryDiagnosticControls {
         telemetryClient.disconnect();
 
         int retryLeft = 3;
-        while (telemetryClient.getOnlineStatus() == false && retryLeft > 0) {
+        boolean isConnectSuccess = telemetryClient.connect(DiagnosticChannelConnectionString);
+        while (isConnectSuccess == false && retryLeft > 0) {
             telemetryClient.connect(DiagnosticChannelConnectionString);
             retryLeft -= 1;
         }
 
-        if (telemetryClient.getOnlineStatus() == false) {
+        if (isConnectSuccess == false) {
             throw new Exception("Unable to connect.");
         }
 
--- src/test/java/com/hsbc/kata/racecar/telemetrysystem/TTelemetryDiagnosticControlsTest.groovy
+++ src/test/java/com/hsbc/kata/racecar/telemetrysystem/TTelemetryDiagnosticControlsTest.groovy
@@ -10,7 +10,6 @@ class TTelemetryDiagnosticControlsTest extends Specification {
         setup:
         def result = "LAST TX rate................ 100 MBPS\r\n" + "HIGHEST TX rate............. 100 MBPS\r\n" + "LAST RX rate................ 100 MBPS\r\n" + "HIGHEST RX rate............. 100 MBPS\r\n" + "BIT RATE.................... 100000000\r\n" + "WORD LEN.................... 16\r\n" + "WORD/FRAME.................. 511\r\n" + "BITS/FRAME.................. 8192\r\n" + "MODULATION TYPE............. PCM/FM\r\n" + "TX Digital Los.............. 0.75\r\n" + "RX Digital Los.............. 0.10\r\n" + "BEP Test.................... -5\r\n" + "Local Rtrn Count............ 00\r\n" + "Remote Rtrn Count........... 00"
         telemetryClient.disconnect() >> false
-        telemetryClient.getOnlineStatus() >> true
         telemetryClient.connect(telemetryDiagnosticControls.getDiagnosticChannelConnectionString()) >> true
         telemetryClient.send(TelemetryClient.DIAGNOSTIC_MESSAGE) >> result
         telemetryClient.receive() >> result
