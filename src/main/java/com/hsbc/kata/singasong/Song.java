bf2d96da0ddf4e8c7e18cdf502c278c2b2100a41
zach007 <zach007cn@gmail.com>
2019-04-17 22:59:37 +0800

    sing a song kata

--- src/main/java/com/hsbc/kata/singasong/Song.java
+++ src/main/java/com/hsbc/kata/singasong/Song.java
@@ -1,109 +1,84 @@
 package com.hsbc.kata.singasong;
 
-import com.google.common.base.Strings;
 import lombok.Getter;
 
 import java.util.List;
 
 public class Song {
+    public static final String suffixString = "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+            "\n";
+
+    public static void main(String[] args) {
+
+        String song = Animal.fly.verse +
+                suffixString +
+                Animal.spider.verse +
+                "She swallowed the spider to catch the fly;\n" +
+                suffixString +
+                Animal.bird.verse +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                suffixString +
+                Animal.cat.verse +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                suffixString +
+                Animal.dog.verse +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                suffixString +
+                Animal.cow.verse +
+                "She swallowed the cow to catch the dog,\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                suffixString +
+                Animal.horse.verse;
+
+        System.out.println(song);
+    }
+
     public String lyricsFor(List<String> animal) {
         if (animal.isEmpty()) {
             return "";
         } else if (animal.size() == 1) {
             String oneAnimal = animal.get(0);
             if (oneAnimal.equalsIgnoreCase(Animal.fly.name())) {
-                return prefixString + Animal.fly.name() + middleString + Animal.fly.name() + suffixString;
+                return Animal.fly.verse + suffixString;
             }
         } else {
-            StringBuilder sb = new StringBuilder();
             Animal[] values = Animal.values();
-            for (int i = 0; i < values.length; i++) {
-                Animal value = values[i];
-
-                String bar = "";
-
-                if (i - 1 >= 0) {
-                    Animal beforeValue = values[i - 1];
-                    bar = beforeValue.name();
-                }
-                if (value.name().equals(animal.get(i))) {
-                    sb.append(prefixString)
-                            .append(value.name())
-                            .append(value.verse)
-                            //todo
-                            .append(foo(value.name(), bar))
-                            .append(middleString)
-                            .append(value.name())
-                            .append(suffixString);
+            for (int i = 0; i < animal.size(); i++) {
+                String name = animal.get(i);
+                for (int j = 0; j < values.length; j++) {
+                    Animal value = values[j];
+                    if (name.equals(value)) {
+                        return value.verse + suffixString;
+                    }
                 }
             }
-            return sb.toString();
         }
         return null;
     }
 
-    private String foo(String animal, String beforeValue) {
-        if (!Strings.isNullOrEmpty(beforeValue)) {
-            return String.format("She swallowed the %s to catch the %s", animal, beforeValue);
-        }
-        {
-            return "";
-        }
-    }
-
-    private static final String prefixString = "There was an old lady who swallowed a ";
-    private static final String middleString = ".\nI don't know why she swallowed a ";
-    private static final String suffixString = " - perhaps she'll die!\n" + "\n";
-
-    public static void main(String[] args) {
-
-        String song = "There was an old lady who swallowed a fly.\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a spider;\n" +
-                "That wriggled and wiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a bird;\n" +
-                "How absurd to swallow a bird.\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a cat;\n" +
-                "Fancy that to swallow a cat!\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a dog;\n" +
-                "What a hog, to swallow a dog!\n" +
-                "She swallowed the dog to catch the cat,\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a cow;\n" +
-                "I don't know how she swallowed a cow!\n" +
-                "She swallowed the cow to catch the dog,\n" +
-                "She swallowed the dog to catch the cat,\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a horse...\n" +
-                "...She's dead, of course!";
-
-        System.out.println(song);
-    }
-
     enum Animal {
-        fly(""),
-        spider("\nThat wriggled and wiggled and tickled inside her.\n");
+        fly("There was an old lady who swallowed a fly.\n"),
+        spider("There was an old lady who swallowed a spider;\n" +
+                "That wriggled and wiggled and tickled inside her.\n"),
+        bird("There was an old lady who swallowed a bird;\n" +
+                "How absurd to swallow a bird.\n"),
+        cat("There was an old lady who swallowed a cat;\n" +
+                "Fancy that to swallow a cat!\n"),
+        dog("There was an old lady who swallowed a dog;\n" +
+                "What a hog, to swallow a dog!\n"),
+        cow("There was an old lady who swallowed a cow;\n" +
+                "I don't know how she swallowed a cow!\n"),
+        horse("There was an old lady who swallowed a horse...\n" +
+                "...She's dead, of course!");
 
         @Getter
         private String verse;
